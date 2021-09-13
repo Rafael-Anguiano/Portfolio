7eaaa3209fec4be92bdde8e79f5311f068967b5d
Rafael Anguiano <rj.anguiano.23@gmail.com>
2021-09-13 00:36:16 -0500

    Finishing intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,33 @@
 import "./intro.scss"
+import {init} from 'ityped'
+import { useEffect, useRef } from "react"
 
-export default function intro() {
+export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current,{
+            showCursor: true,
+            backDelay: 2000,
+            backSpeed: 60,
+            strings: ["Web Developer 💻", "Pets lover 🐕", "Mobile Developer 📱", "Gamer 🎮" ]
+        });
+    }, []);
     return (
         <div  className="intro" id="intro">
-            intro
+            <div className="left">
+                <img className="imgContainer" src="assets/man.png" alt=""/>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Rafael Anguiano</h1>
+                    <h3><span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
