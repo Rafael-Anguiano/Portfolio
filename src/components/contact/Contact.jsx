6d7f57dd2593fc02cb609548e448755b22c38a99
Rafael Anguiano <rj.anguiano.23@gmail.com>
2021-10-18 01:22:40 -0500

    Part of the first Version

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,7 @@
 import "./contact.scss"
 import { useState } from "react";
+import {Person, Mail, GitHub, Phone, LinkedIn} from "@material-ui/icons"
+import { Link } from "@material-ui/core";
 
 export default function Contact() {
 
@@ -16,12 +18,41 @@ export default function Contact() {
             </div>
             <div className="right">
                 <h2>Contact.</h2>
-                <form onSubmit={handleSubmit}>
+                <div className="container" >
+                    <div className="itemContainer">
+                        <a href="https://wa.me/+523331504871" target="_blank">
+                            <Phone className="icon"/>
+                            <span>+52 1 333 150 4871</span>
+                        </a>
+                    </div>
+
+                    <div className="itemContainer">
+                        <a href="mailto:rj.anguiano.23@gmail.com" target="_blank">
+                            <Mail className="icon"/>
+                            <span>rj.anguiano.23@gmail.com</span>
+                        </a>
+                    </div>
+
+                    <div className="itemContainer">
+                        <a href="https://www.linkedin.com/in/rafael-anguiano/" target="_blank">
+                            <LinkedIn className="icon"/>
+                            <span>Rafael Anguiano</span>
+                        </a>
+                    </div>
+                    <div className="itemContainer">
+                        <a href="https://github.com/Rafael-Anguiano" target="_blank">
+                            <GitHub className="icon"/>
+                            <span>Repositories / Projects</span>
+                        </a>
+                    </div> 
+                </div>
+                
+                {/* <form onSubmit={handleSubmit}>
                     <input type="text" placeholder="Email" />
                     <textarea placeholder="Message"></textarea>
                     <button type="submit">Send</button>
                     {message && <span> Thanks, I'll reply as soon as posible! 😁</span> }
-                </form>
+                </form> */}
             </div>
         </div>
     )
